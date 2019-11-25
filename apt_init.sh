b126ed4a5b63d7a3862bf32d4b1346fe90951831
jch8828 <chjz8828@gmail.com>
2019-11-24 20:29:41 -0800

    Update apt_init

--- apt_init.sh
+++ apt_init.sh
@@ -53,5 +53,10 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
 source ~/.zshrc
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
+source ~/.zshrc
+
+
 
 
